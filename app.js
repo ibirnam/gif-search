181593ee02b3b92709de3be6e9ef73ec65e33cde
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-05-08 11:08:16 -0700

    P03 - Tenor Revamp

--- app.js
+++ app.js
@@ -10,15 +10,28 @@ const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+const Tenor = require("tenorjs").client({
+    "Key": "GI44QX5W54NP", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // Routes
 app.get('/', (req, res) => {
-    console.log(req.query) // => "{ term: hey" }
-    res.render('home')
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
 })
-// app.get('/', (req, res) => {
-//     const gifUrl = 'https://media.giphy.com/media/26ufcfqqe6Fobdygg/giphy.gif'
-//     res.render('hello-gif', { gifUrl })
-// })
 
 app.get('/greetings/:name', (req, res) => {
     const name = req.params.name;
